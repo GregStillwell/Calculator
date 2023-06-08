b850e97554ae4bf623ac1673b623d52cfa07ee0a
Greg <Jariye05@gmail.com>
2023-06-07 19:47:24 -0700

    Project setup

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// BUSINESS LOGIC
+function add(number1, number2) {
+  return number1 + number2;
+ }
+
+// USER INTERFACE LOGIC
+window.alert(add(10,5));
+const number1 = parseInt("Enter a number")
+const number2 = parseInt("Enter another number");
+
+window.alert(add(number1,number2));
+
